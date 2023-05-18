15783b5611f5d53ccbdc08ffff39c768034de61e
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-05-18 18:35:05 -0300

    formulario para cadastro das tarefas

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,27 @@
       <h1>Minhas Tarefas</h1>
       <p>Você possui 7 tarefas pendentes</p>
     </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input
+            type="text"
+            placeholder="Digite a descrição da tarefa"
+            class="form-control"
+          />
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-2">
+          <select class="form-control">
+            <option value="todas">Todas as tarefas</option>
+            <option value="pendentes">Tarefas pendentes</option>
+            <option value="finalizadas">Tarefas finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
